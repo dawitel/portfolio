aa38d441245f599082845a42c3492345b0804b1b
dawitel <dawiteliaskassaye@gmail.com>
2024-08-19 04:16:27 +0300

    update: workflow

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import { TextGenerateEffect } from "@/components/ui/TextGenerateEffect";
 const Blogpage = () => {
   return (
     <>
-      <main className="relative bg-black-100 flex justify-center h-full items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <main className="bg-black-100 flex justify-center mt-64 h-full items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <p className="uppercase items-center justify-center flex tracking-widest text-[10px] text-center text-blue-100 max-w-80">
           Stay tuned for the updates
         </p>
